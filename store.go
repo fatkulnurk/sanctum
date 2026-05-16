36dddbae59709e98647e84282a31c80a1b344c7d
fatkulnurk <fatkulnurk@gmail.com>
2026-05-16 16:20:25 +0700

    feat: make Store.FindByID use IDType and add PruneExpired

--- store.go
+++ store.go
@@ -8,7 +8,8 @@ import (
 type Store[IDType, TokenableIDType comparable] interface {
 	Create(ctx context.Context, t *PersonalAccessToken[IDType, TokenableIDType]) error
 	FindByToken(ctx context.Context, hashedToken string) (*PersonalAccessToken[IDType, TokenableIDType], error)
-	FindByID(ctx context.Context, id string) (*PersonalAccessToken[IDType, TokenableIDType], error)
+	FindByID(ctx context.Context, id IDType) (*PersonalAccessToken[IDType, TokenableIDType], error)
 	Delete(ctx context.Context, id IDType) error
 	UpdateLastUsedAt(ctx context.Context, id IDType, at time.Time) error
+	PruneExpired(ctx context.Context, beforeTime time.Time) (int64, error)
 }
